62a71c9d12e6a15e8d4919250bc7f34cab901a8a
Philippe <phillclesco@yahoo.com>
2019-06-09 02:26:52 -0400

    A LOAD of changes and improvements

--- src/js/main.js
+++ src/js/main.js
@@ -1,36 +1,93 @@
-const NotFound = { template: '<p>Page not found</p>' };
-const Home = { template: '<p>about page</p>' }
-
-const About = { template: '<p>about page</p>' }
-const routes = {
-  '/': Home,
-  '/about': About
+var navBColor = "#0C0C0C";
+var navBarHeight = $('.menu-row1').height();
+var viewHeight = $(document).outerHeight();
+var pgLvl = {
+    'home' : 0,
+    'about-me' : 100,
+    'projects' : 200,
+    'accolades' : 300,
+    'contact' : 400
 }
 
-var app = new Vue({
-    el: '#app',
-    data: {
-        message: 'Hello Vue!',
-        currentRoute: window.location.pathname
-    },
-    computed: {
-        ViewComponent () {
-            return routes[this.currentRoute] || NotFound
+$('#nav-open').on('click', () => {
+    navOpen();
+});
+
+function navOpen(){
+    anime({
+        targets: '#full-menu',
+        backgroundColor: navBColor,
+        easing: 'easeInOutQuint',
+        top: `0px`,
+        begin: function(anim){
+            $('#full-menu').toggleClass('hidden-item');
         }
-    },
-    render (h) { return h(this.ViewComponent) }
+    });
+    anime({
+        targets: "#nav-bar",
+        //bottom: `-${navBarHeight}px`,
+        opacity: '1',
+        delay: 700,
+        easing: 'easeInOutQuad'
+    });
+}
+
+$('#nav-open').hover(() =>{
+    anime({
+        targets: '#nav-open-back',
+        marginRight: '0.5em',
+        easing: 'easeInOutQuad',
+        duration: 100
+    })
+}, () =>{
+    anime({
+        targets: '#nav-open-back',
+        marginRight: '0em',
+        easing: 'easeInOutQuad',
+        duration: 100
+    })
+});
+
+$('#nav-close').on('click', () => {
+    navClose();
+});
+function navClose(){
+    anime({
+        targets: '#full-menu',
+        backgroundColor: '#FFF',
+        easing: 'easeInOutQuint',
+        top: `-${viewHeight}px`,
+        delay: 700,
+        complete: function(anim){
+            $('#full-menu').toggleClass('hidden-item');
+        }
+    });
+    anime({
+        targets: "#nav-bar",
+        //bottom: `-${navBarHeight}px`,
+        opacity: '0',
+        easing: 'easeInOutQuad'
+    })
+}
+
+$('.menu-link').on('click', () => {
+    navClose();
+});
+
+$('#nav-about-me').on('click', () => {
+    $('#content').load('./aboutme.html');
+});
+$('#nav-skills').on('click', () => {
+    $('#content').load('./skills.html');
+});
+$('#nav-projects').on('click', () => {
+    $('#content').load('./projects.html');
+});
+$('#nav-contact').on('click', () => {
+    $('#content').load('./contact.html');
 });
 
 
-Vue.use(VueRouter);
-
-const router = new VueRouter({
-    routes: [
-        {
-            path: '/',
-            name: 'home',
-            template: '<Home/>',
-            component: Home
-        }
-    ]
+$(document).ready(function() {
+    $('#content').load('./home.html');
 });
